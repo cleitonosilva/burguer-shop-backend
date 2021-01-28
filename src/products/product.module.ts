c695aceb3f2a644d690d51b2ba4e80b98c2544d7
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-01-28 14:57:41 -0300

    correção de import do controller

--- src/products/product.module.ts
+++ src/products/product.module.ts
@@ -1,10 +1,11 @@
-import { ProductService } from './shared/product.service';
 import { Module } from '@nestjs/common';
+
 import { ProductsController } from './products.controller';
+import { ProductService } from './shared/product.service';
 
 @Module({
-    imports: [ProductsController],
-    controllers: [],
+    imports: [],
+    controllers: [ProductsController],
     providers: [ProductService],
 })
 export class ProductModule {}
