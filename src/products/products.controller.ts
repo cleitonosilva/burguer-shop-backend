6c569c181a67c70e9e913f67868b9e12b26c290f
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-01-28 14:58:18 -0300

    primeiros metodos get com e sem id

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Product } from './shared/product';
+import { ProductService } from './shared/product.service';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+
+
+    constructor(
+    private productService: ProductService,
+    ){ }
+
+@Get()
+async getAll() : Promise<Product[]>{
+     return this.productService.getAll()
+}
+
+@Get(':id')
+async getbyId(@Param('id') id : number) : Promise<Product>{
+     return this.productService.getbyId(id);
+}
+
+
+
+}
