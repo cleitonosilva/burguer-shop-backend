b79e4c7f4cd8eaa8810ad0597e67940d34136ea9
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-01-28 14:28:40 -0300

    Classe product

--- src/products/shared/product.ts
+++ src/products/shared/product.ts
@@ -1,1 +1,11 @@
-export class Product {}
+export class Product {
+    grif: string;
+    categoria: string;
+    produto: string;
+    descricao: string;
+    preco: number;
+    url: string;
+    id: number;
+    quantidade: number;
+    valorTotal: number;
+}
