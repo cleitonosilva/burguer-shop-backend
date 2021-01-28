52e4173951573297642e87a97ae6351d7ce0e5a9
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-01-28 17:54:28 -0300

    Injetando depencias

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { MongooseModule } from '@nestjs/mongoose'
+
+
 @Module({
-  imports: [ProductModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://ttom0202:<ttom0202>@cluster0.5rn2u.mongodb.net/<Burguer>?retryWrites=true&w=majority'),
+    ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/products/product.module.ts
+++ src/products/product.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { ProductsController } from './products.controller';
 import { ProductService } from './shared/product.service';
+import { ProductSchema } from './shared/schemas/product.schema';
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-    imports: [],
+    imports: [
+        MongooseModule.forFeature([{name: 'Product', schema: ProductSchema}])
+        ],
     controllers: [ProductsController],
     providers: [ProductService],
 })
