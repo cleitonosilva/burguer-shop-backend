b5131b0a4132645ffe082abaee53e83975ce7d22
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-01-28 14:57:56 -0300

    dependencia do controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ProductModule, ],
+  imports: [ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
