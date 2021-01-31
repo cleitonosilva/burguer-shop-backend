42891dbe35f5e6b6f41b2776b988632f0cfb7275
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-01-30 23:30:31 -0300

    Classe usuario alterado

--- src/users/shared/user.ts
+++ src/users/shared/user.ts
@@ -2,5 +2,10 @@ export class User {
     id: number;
     email: string;
     senha: string;
-   
+    endereco: string;
+    telefone: string;
+    pontoReferencia: string;
+    confirmarSenha: string;
+    sobrenome: string;
+    nome: string;
 }
