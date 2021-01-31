b30cb5308fdc4a3732e71d66e32f68ba093ed69f
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-01-30 22:34:18 -0300

    importando user Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { ProductModule } from './products/shared/product.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './users/shared/user.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
