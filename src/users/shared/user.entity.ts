4dce2375b6892b967498e615f4491bcb154952ad
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-01-30 22:36:00 -0300

    Entidade de Usuarios

---
+++ src/users/shared/user.entity.ts
@@ -0,0 +1,14 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100 })
+  email: string;
+
+  @Column('text')
+  senha: string;
+
+}
